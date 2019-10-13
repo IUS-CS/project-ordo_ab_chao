9d8e154e8c402989d6ac0bdf1506b9eda5ca7cd4
John Heinz <jdebay@twc.com>
2019-10-13 18:06:29 -0400

    I added and subtracted a lot, this merge will be a pain in the butt

--- src/django_website/django_website/views.py
+++ src/django_website/django_website/views.py
@@ -1,8 +1,3 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-def homepage(request):
-    return render(request, 'homepage.html')
-
-def aboutpage(request):
-    return render(request, 'about.html')
