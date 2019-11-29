ec0609e6755d3c33279d1f2268cdebf5dc2631dd
John Heinz <jdebay@twc.com>
2019-11-28 19:12:33 -0500

    almost done with blog

--- src/django_website/blog/views.py
+++ src/django_website/blog/views.py
@@ -3,8 +3,12 @@ from django.shortcuts import render, get_object_or_404
 from .models import BlogPost
 from .forms import BlogPostModelForm
 
+
 def blog_post_list_view(request):
-    qs = BlogPost.objects.all() # queryset -> list of python objects
+    qs = BlogPost.objects.all().published() # queryset -> list of python objects
+    if request.user.is_authenticated:
+        my_qs = BlogPost.objects.filter(user=request.user)
+        qs = (qs | my_qs).distinct()
     context = {'object_list':qs}
     return render(request, 'blog/list.html', context)
 
