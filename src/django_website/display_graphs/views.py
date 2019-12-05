1c6b2de6f305c2067449c348dd9f7e2bfd8ab861
John Heinz <jdebay@twc.com>
2019-12-04 21:39:47 -0500

    made some changes to the code

--- src/django_website/display_graphs/views.py
+++ src/django_website/display_graphs/views.py
@@ -23,36 +23,21 @@ def display_the_graphs(request):
     y_values = [float(i) for i in y_values]
     x_values_b = content_df['endTime'].tolist()
     x_values = convert_datetime(x_values_b)
-    print('\nx_values: ', x_values,'\n')
-    print('\ny_values: ', y_values,'\n')
-    print('\nx_values count:', len(x_values),'\n')
-    print('\ny_values count:', len(y_values),'\n')
-    print('\nx_values type:', type(x_values[-1]),'\n')
-    print('\ny_values type:', type(y_values[-1]),'\n')
+    #print('\nx_values: ', x_values,'\n')
+    #print('\ny_values: ', y_values,'\n')
+    #print('\nx_values count:', len(x_values),'\n')
+    #print('\ny_values count:', len(y_values),'\n')
+    #print('\nx_values type:', type(x_values[-1]),'\n')
+    #print('\ny_values type:', type(y_values[-1]),'\n')
     chart1_data = list(zip(x_values, y_values))
-    print('chart1 data:', chart1_data)
+    #print('chart1 data:', chart1_data)
     context = {
         'response': content_df.to_html(),
         'content_df': content_df,
         'chart1': chart1_data
     }
     return render(request, 'display_graphs/graphs.html', context)
-'''
-def get_time_graph(request):
-    fig, ax = plt.subplots()
-    ax.set_title('Scatter plot of prices over time')
-    ax.set_xlabel('dates')
-    ax.set_ylabel('sell prices')
-    ax.scatter(content_df.endDate.values, content_df.endPrice.values, s=10, label='sell prices over time')
-    canvas = FigureCanvasAgg(fig)
-    buf = io.BytesIO()
-    plt.savefig(buf, format='png')
-    plt.close(fig)
-    response = HttpResponse(buf.getvalue(), content_type='image/png')
-    canvas.print_png(response)
-    context = {'first_graph':response}
-    return render(request, 'display_graphs/graphs.html', context)
-'''
+
 def to_dict(input_ordered_dict):
     return loads(dumps(input_ordered_dict))
 
@@ -65,10 +50,10 @@ def extract_values(temp_dict):
     f = []
     #print('\ntype of data:\n', type(temp_dict))
     length = len(temp_dict)
-    print('\nlength:\n', length)
+    #print('\nlength:\n', length)
     for index in range(length):
         for key, value in temp_dict[index].items():
-            print('temp_dict[index][key]:', key)
+            #print('temp_dict[index][key]:', key)
             if key == 'itemId':
                 a.append(value)
             if key == 'title':
